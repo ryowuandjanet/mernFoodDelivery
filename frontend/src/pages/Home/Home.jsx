fd1634acc84158c6f2a3baf5fecc31774cfc708e
ryowuandjanet <ryowu0329@gmail.com>
2024-05-29 22:52:27 +0800

    FoodDisplay && FoodItem

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import { FoodDisplay } from "../../components/FoodDisplay/FoodDisplay";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   );
 };
