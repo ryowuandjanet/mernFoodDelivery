1856cbddb1fcde8c5b1940c9f39fa6329654c4ef
ryowuandjanet <ryowu0329@gmail.com>
2024-06-02 16:23:10 +0800

    新增Add List Orders及Sidebar UI調整

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
 
 const App = () => {
   return (
@@ -8,6 +12,11 @@ const App = () => {
       <Navbar />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
   );
