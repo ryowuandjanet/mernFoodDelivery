1c42eb0752af4e6b5f10a647803b3d8627f16671
ryowuandjanet <ryowu0329@gmail.com>
2024-06-02 16:03:25 +0800

    Navbar && Sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
