aa223730451132d68fdcadec4446c27bfad0cb92
ryowuandjanet <ryowu0329@gmail.com>
2024-06-12 01:43:29 +0800

    在frontend目錄下加入.env

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = import.meta.env.VITE_BACKEND_URL;
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
   const addToCart = async (itemId) => {
