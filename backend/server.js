08abb00f2b4c601e20a7e7a7287d9642be8b39b0
ryowuandjanet <ryowu0329@gmail.com>
2024-06-13 23:41:41 +0800

    add CORS in server

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,20 @@ app.use(
   })
 );
 
+// 處理 CORS
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://mern-food-delivery-admin-beige.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 connectDB();
 
 app.use("/api/food", foodRouter);
